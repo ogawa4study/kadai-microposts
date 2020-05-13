c4a769cc5d69fb280a66c31f5a972b7f33cc1399
ogawa4study <ogawa4study@gmail.com>
2020-05-13 10:00:18 +0000

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('id', 'desc')->paiginate(10);
+        
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+    
+    public function show($id)
+    {
+        $user = User::find($id);
+        
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+    
+    
+    
+    
+}
+
